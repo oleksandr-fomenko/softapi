bb16cb37e357a8e23e37f37be1b3a441a2feed51
Oleksandr_Fomenko <Oleksandr_Fomenko>
2021-06-08 17:14:10 +0300

    added base attributes tests

---
+++ SoftAPI/SoftAPIClient.Tests/BaseImplOfParameterAttributeTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using SoftAPIClient.Attributes;
+using SoftAPIClient.Attributes.Base;
+using System.Collections.Generic;
+
+namespace SoftAPIClient.Tests
+{
+    public class BaseImplOfParameterAttributeTests : AbstractTest
+    {
+
+        [TestCaseSource(nameof(GetTestData))]
+        public void VerifyValue(KeyValuePair<string, BaseParameterAttribute> testData)
+        {
+            var expectedValue = testData.Key;
+            var parameter = testData.Value;
+
+            Assert.AreEqual(expectedValue, parameter.Value);
+        }
+
+        private static IEnumerable<KeyValuePair<string, BaseParameterAttribute>> GetTestData()
+        {
+            yield return new KeyValuePair<string, BaseParameterAttribute>("testPathParameter", new PathParameterAttribute("testPathParameter"));
+            yield return new KeyValuePair<string, BaseParameterAttribute>("testReplaceableParameter", new ReplaceableParameterAttribute("testReplaceableParameter"));
+            yield return new KeyValuePair<string, BaseParameterAttribute>("testQueryParameter", new QueryParameterAttribute("testQueryParameter"));
+            yield return new KeyValuePair<string, BaseParameterAttribute>("testFormDataParameter", new FormDataParameterAttribute("testFormDataParameter"));
+            yield return new KeyValuePair<string, BaseParameterAttribute>("testHeaderParameter", new HeaderParameterAttribute("testHeaderParameter"));
+            yield return new KeyValuePair<string, BaseParameterAttribute>("testLog", new LogAttribute("testLog"));
+        }
+    }
+}
---
+++ SoftAPI/SoftAPIClient.Tests/BodyAttributeTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SoftAPIClient.Attributes;
+using SoftAPIClient.MetaData;
+
+namespace SoftAPIClient.Tests
+{
+    public class BodyAttributeTests : AbstractTest
+    {
+        [Test]
+        public void VerifyDefaultBodyType()
+        {
+            var bodyAttribute = new BodyAttribute();
+            Assert.AreEqual(BodyType.Json, bodyAttribute.BodyType);
+            Assert.IsNull(bodyAttribute.Value);
+        }
+
+        [Test]
+        public void VerifyBodyType()
+        {
+            var bodyAttribute = new BodyAttribute(BodyType.Xml);
+            Assert.AreEqual(BodyType.Xml, bodyAttribute.BodyType);
+            Assert.IsNull(bodyAttribute.Value);
+        }
+    }
+}
