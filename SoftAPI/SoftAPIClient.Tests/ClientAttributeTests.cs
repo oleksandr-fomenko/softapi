2b4d7ab4805d5559dfef3eaeaad00723856a788b
Oleksandr_Fomenko <Oleksandr_Fomenko>
2021-06-08 18:03:58 +0300

    added ClientAttribute tests

---
+++ SoftAPI/SoftAPIClient.Tests/ClientAttributeTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using SoftAPIClient.Attributes;
+using System;
+
+namespace SoftAPIClient.Tests
+{
+    public class ClientAttributeTests : AbstractTest
+    {
+        [Test]
+        public void VerifyDefaultValue()
+        {
+            var clientAttribute = new ClientAttribute();
+            Assert.IsNull(clientAttribute.ResponseConverterType);
+            Assert.IsNull(clientAttribute.Url);
+            Assert.AreEqual(string.Empty, clientAttribute.DynamicUrlKey);
+            Assert.IsNull(clientAttribute.Logger);
+            Assert.IsNull(clientAttribute.DynamicUrlType);
+            Assert.IsNull(clientAttribute.RequestInterceptor);
+            Assert.IsNull(clientAttribute.ResponseInterceptors);
+            Assert.AreEqual(string.Empty, clientAttribute.Path);
+        }
+
+        [Test]
+        public void VerifyValueWithTypeValue()
+        {
+            var type = typeof(Type);
+            var clientAttribute = new ClientAttribute(type);
+            Assert.AreEqual(type, clientAttribute.ResponseConverterType);
+            Assert.IsNull(clientAttribute.Url);
+            Assert.AreEqual(string.Empty, clientAttribute.DynamicUrlKey);
+            Assert.IsNull(clientAttribute.Logger);
+            Assert.IsNull(clientAttribute.DynamicUrlType);
+            Assert.IsNull(clientAttribute.RequestInterceptor);
+            Assert.IsNull(clientAttribute.ResponseInterceptors);
+            Assert.AreEqual(string.Empty, clientAttribute.Path);
+        }
+
+    }
+}
