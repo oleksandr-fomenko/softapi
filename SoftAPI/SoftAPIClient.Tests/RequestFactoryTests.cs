8cdcca591deff35f308f5117f2bd30780d7782b0
Oleksandr_Fomenko <Oleksandr_Fomenko>
2021-06-09 12:22:03 +0300

    added tests for reqest factory

--- SoftAPI/SoftAPIClient.Tests/RequestFactoryTests.cs
+++ SoftAPI/SoftAPIClient.Tests/RequestFactoryTests.cs
@@ -2,15 +2,17 @@
 using SoftAPIClient.Attributes;
 using SoftAPIClient.Core;
 using SoftAPIClient.Core.Exceptions;
+using SoftAPIClient.Core.Interfaces;
 using SoftAPIClient.MetaData;
 using System;
+using System.Linq;
 
 namespace SoftAPIClient.Tests
 {
     public class RequestFactoryTests : AbstractTest
     {
         [Test]
-        public void VerifyNullUrlException()
+        public void VerifyInitializationExceptionWhenNullUrlProvided()
         {
             var targetInterface = typeof(ITestInterface);
             var methodName = "Get";
@@ -22,6 +24,31 @@ namespace SoftAPIClient.Tests
             Assert.AreEqual($"The result URL is not defined at the interface '{nameof(ITestInterface)}' and method '{methodName}'", ex.Message);
         }
 
+        [Test]
+        public void VerifyInitializationExceptionWhenDifferentArgumentsProvided()
+        {
+            var targetInterface = typeof(ITestInterface);
+            var methodName = "Get";
+            var arguments = new[] { "1" , "2"};
+
+            var requestFactory = new RequestFactory(targetInterface, targetInterface.GetMethod(methodName), arguments);
+
+            var ex = Assert.Throws<InitializationException>(() => requestFactory.BuildRequest());
+            Assert.AreEqual($"Argument count '{2}' and MethodInfo count '{1}' " +
+                    $"is not matched for the method '{methodName}' in type '{nameof(ITestInterface)}'", ex.Message);
+        }
+
+        [Test]
+        public void VerifyDefaultResponseInterceptors()
+        {
+            var targetInterface = typeof(ITestInterface);
+            var methodName = "Get";
+            var arguments = new[] { "1" };
+
+            var requestFactory = new RequestFactory(targetInterface, targetInterface.GetMethod(methodName), arguments);
+
+            Assert.AreEqual(Enumerable.Empty<IResponseInterceptor>(), requestFactory.ResponseInterceptors);
+        }
 
         [Client]
         private interface ITestInterface
