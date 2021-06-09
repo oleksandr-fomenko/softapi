50034a809226231c7de6d7219bd45a0d56dcb657
Oleksandr_Fomenko <Oleksandr_Fomenko>
2021-06-09 16:48:38 +0300

    added fix for subclasses and added additional test

--- SoftAPI/SoftAPIClient.Tests/RestClientTests.cs
+++ SoftAPI/SoftAPIClient.Tests/RestClientTests.cs
@@ -16,6 +16,13 @@ namespace SoftAPIClient.Tests
             Assert.AreEqual($"Provided type '{typeof(IInterfaceWithoutClientAttribute).Name}' must be annotated with '{typeof(ClientAttribute).Name}' attribute", ex.Message);
         }
 
+        [Test]
+        public void VerifyInitializationExceptionWhenNoConvertersProvided()
+        {
+            var ex = Assert.Throws<InitializationException>(() => RestClient.Instance.GetService<ITestInterface>().Get("1").Invoke());
+            Assert.AreEqual($"There is no registered convertors found for the '{typeof(RestClient).Name}'. Please add at least one of it.", ex.Message);
+        }
+
     }
 
     public interface IInterfaceWithoutClientAttribute
