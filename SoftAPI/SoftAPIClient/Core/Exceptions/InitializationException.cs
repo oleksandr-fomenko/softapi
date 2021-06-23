f4f452b6768e59b2c32b8a5f60f4b1aeea6bf29b
Oleksandr_Fomenko <Oleksandr_Fomenko>
2021-06-23 23:36:46 +0300

    Refactored InitializationException

--- SoftAPI/SoftAPIClient/Core/Exceptions/InitializationException.cs
+++ SoftAPI/SoftAPIClient/Core/Exceptions/InitializationException.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace SoftAPIClient.Core.Exceptions
 {
-    [Serializable]
-    public sealed class InitializationException : Exception
+    public class InitializationException : Exception
     {
         public InitializationException(string message) : base(message)
         {
-        }
-
-        private InitializationException(SerializationInfo info, StreamingContext context)
-        : base(info, context)
-        {
-        }
-
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            base.GetObjectData(info, context);
         }
     }
 }
