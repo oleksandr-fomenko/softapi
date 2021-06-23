a8ce1d511a78f5447ed803ee30d8487a2616149a
Oleksandr_Fomenko <Oleksandr_Fomenko>
2021-06-23 17:30:38 +0300

    Added tests for RestSharpResponseConverter

---
+++ SoftAPI/SoftAPIClient.RestSharpNewtonsoft.Tests/RestSharpResponseConverterConvertTests.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using SoftAPIClient.MetaData;
+using SoftAPIClient.Core;
+
+namespace SoftAPIClient.RestSharpNewtonsoft.Tests
+{
+    [TestFixture]
+    public class RestSharpResponseConverterConvertTests : RestSharpResponseConverter
+    {
+        private static readonly ResponseTests.UserJsonDto UserObject = new ResponseTests.UserJsonDto
+        {
+            Age = 23,
+            Name = "JsonLad"
+        };
+        private static readonly string SerializedBody = JsonConvert.SerializeObject(UserObject);
+        private const string Url = "http://localhost:8080";
+        private static readonly Uri Uri = new Uri(Url);
+        private const HttpStatusCode StatusCode = HttpStatusCode.OK;
+        private const string Method = "POST";
+        private const string ContentType = "application/json";
+        private const string Path = "/path";
+        private static readonly Dictionary<string, object> PathParameters = new Dictionary<string, object> { { "path_int_key", 1 }, { "path_string_key", "string" }, { "path_null_key", null } };
+        private static readonly Dictionary<string, object> QueryParameters = new Dictionary<string, object> { { "query_int_key", 1 }, { "query_string_key", "string" }, { "query_null_key", null } };
+        private static readonly Dictionary<string, object> FormDataParameters = new Dictionary<string, object> { { "formData_int_key", 1 }, { "formData_string_key", "string" } };
+        private static readonly List<KeyValuePair<string, string>> Headers = new List<KeyValuePair<string, string>>
+        {
+            new KeyValuePair<string, string>("Content-Type", ContentType),
+            new KeyValuePair<string, string>("x-api-key", null),
+        };
+        private static readonly List<KeyValuePair<string, string>> Cookies = new List<KeyValuePair<string, string>>
+        {
+            new KeyValuePair<string, string>("Cookie1", "1"),
+            new KeyValuePair<string, string>("Cookie2", null)
+        };
+        private static readonly KeyValuePair<BodyType, object> Body = new KeyValuePair<BodyType, object>(BodyType.Json, UserObject);
+        private static readonly RestSharpJsonResponseDeserializer Deserializer = new RestSharpJsonResponseDeserializer();
+        private static readonly DynamicRequestSettings DynamicRequestSettings = new DynamicRequestSettings();
+
+        private RestResponse _restResponse;
+
+        [TestCaseSource(nameof(GetTestData))]
+        public void VerifyConvertRequest(Tuple<RestResponse, Request, Response> input)
+        {
+            SetRestResponse(input.Item1);
+            var expectedResponse = input.Item3;
+            var actualResponse = Convert(() => input.Item2);
+
+            Assert.AreEqual(expectedResponse.HttpStatusCode, actualResponse.HttpStatusCode);
+            Assert.AreEqual(expectedResponse.ResponseUri, actualResponse.ResponseUri);
+            Assert.AreEqual(expectedResponse.ContentType, actualResponse.ContentType);
+            Assert.AreEqual(expectedResponse.OriginalRequest, actualResponse.OriginalRequest);
+            Assert.AreEqual(expectedResponse.ResponseBodyString, actualResponse.ResponseBodyString);
+            Assert.AreEqual(expectedResponse.Headers, actualResponse.Headers);
+            Assert.AreEqual(expectedResponse.Cookies, actualResponse.Cookies);
+            Assert.IsNotNull(actualResponse.Deserializer);
+            Assert.IsInstanceOf<RestSharpJsonResponseDeserializer>(actualResponse.Deserializer);
+        }
+
+        protected override IRestResponse GetRestResponse(IRestClient client, RestRequest restRequest)
+        {
+            return _restResponse;
+        }
+
+        private static IEnumerable<Tuple<RestResponse, Request, Response>> GetTestData()
+        {
+            var headersRestRequest = new List<Parameter>
+            {
+                new Parameter("Content-Type", ContentType, ParameterType.HttpHeader),
+                new Parameter("x-api-key", null, ParameterType.HttpHeader),
+            };
+
+            var cookiesRestRequest = new List<RestResponseCookie>
+            {
+                new RestResponseCookie
+                {
+                    Name = "Cookie1",
+                    Value = "1"
+                },
+                new RestResponseCookie
+                {
+                    Name = "Cookie2",
+                    Value = null
+                }
+            };
+
+            var restResponse = new RestResponse
+            {
+                StatusCode = StatusCode,
+                ResponseUri = Uri,
+                ContentType = ContentType,
+                Content = SerializedBody
+            };
+            SetInternalProps(restResponse, headersRestRequest, cookiesRestRequest);
+            var request = new Request
+            {
+                Url = Url,
+                Method = Method,
+                Path = Path,
+                PathParameters = PathParameters,
+                QueryParameters = QueryParameters,
+                FormDataParameters = FormDataParameters,
+                Headers = Headers,
+                Body = Body,
+                Deserializer = Deserializer,
+                Settings = DynamicRequestSettings
+            };
+            var expectedResponse = new Response
+            {
+                HttpStatusCode = StatusCode,
+                ResponseUri = Uri,
+                Headers = Headers,
+                Cookies = Cookies,
+                ContentType = ContentType,
+                OriginalRequest = request,
+                OriginalResponse = restResponse,
+                ResponseBodyString = SerializedBody,
+                Deserializer = Deserializer
+            };
+
+            yield return new Tuple<RestResponse, Request, Response>(restResponse, request, expectedResponse);
+
+
+            restResponse = new RestResponse
+            {
+                StatusCode = StatusCode,
+                ResponseUri = Uri,
+                ContentType = ContentType,
+                Content = null
+            };
+            SetInternalProps(restResponse);
+            request = new Request
+            {
+                Url = Url,
+                Method = Method,
+                Path = Path,
+                PathParameters = new Dictionary<string, object>(),
+                QueryParameters = new Dictionary<string, object>(),
+                FormDataParameters = new Dictionary<string, object>(),
+                Headers = new List<KeyValuePair<string, string>>(),
+                Body = default,
+                Deserializer = null,
+                Settings = null
+            };
+            expectedResponse = new Response
+            {
+                HttpStatusCode = StatusCode,
+                ResponseUri = Uri,
+                Headers = new List<KeyValuePair<string, string>>(),
+                Cookies = new List<KeyValuePair<string, string>>(),
+                ContentType = ContentType,
+                OriginalRequest = request,
+                OriginalResponse = restResponse,
+                ResponseBodyString = string.Empty,
+                Deserializer = null
+            };
+
+            yield return new Tuple<RestResponse, Request, Response>(restResponse, request, expectedResponse);
+        }
+
+        private void SetRestResponse(RestResponse restResponse)
+        {
+            _restResponse = restResponse;
+        }
+
+        private static void SetInternalProps(RestResponse restResponse, IList<Parameter> restHeaders = null, IList<RestResponseCookie> restCookies = null)
+        {
+            var type = typeof(RestResponse);
+            if (restHeaders != null)
+            {
+                type.GetProperty("Headers")?.SetValue(restResponse, restHeaders);
+            }
+            if (restCookies != null)
+            {
+                type.GetProperty("Cookies")?.SetValue(restResponse, restCookies);
+            }
+        }
+
+    }
+}
