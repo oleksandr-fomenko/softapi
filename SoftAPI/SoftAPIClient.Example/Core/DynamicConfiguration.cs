855a3868b955a42c1a0190d46bde39b74fc1c47a
Oleksandr_Fomenko <Oleksandr_Fomenko>
2021-06-29 15:32:20 +0300

    small fix

--- SoftAPI/SoftAPIClient.Example/Core/DynamicConfiguration.cs
+++ SoftAPI/SoftAPIClient.Example/Core/DynamicConfiguration.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Configuration;
-using SoftAPIClient.Core.Interfaces;
+﻿using SoftAPIClient.Core.Interfaces;
 
 namespace SoftAPIClient.Example.Core
 {
@@ -8,9 +6,7 @@ namespace SoftAPIClient.Example.Core
     {
         public string GetUrl(string key)
         {
-           
             return CustomConfigurationManager.Configuration[key];
-           // throw new NotImplementedException();
         }
     }
 }
