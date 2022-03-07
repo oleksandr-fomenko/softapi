fa490b35d4167769a199e6cb62795a5cdfc90b30
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-07 12:22:32 +0200

    changed VerifyInitializationExceptionMessage to cover protectable constructor

--- SoftAPI/SoftAPIClient.Tests/InitializationExceptionTests.cs
+++ SoftAPI/SoftAPIClient.Tests/InitializationExceptionTests.cs
@@ -1,27 +1,37 @@
-﻿using System.Runtime.Serialization;
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using NUnit.Framework;
 using SoftAPIClient.Core.Exceptions;
 
 namespace SoftAPIClient.Tests
 {
-    public class InitializationExceptionTests : InitializationException
+    public class InitializationExceptionTests : AbstractTest
     {
-        public InitializationExceptionTests() : base(nameof(InitializationExceptionTests))
-        {
-        }
-
-        public InitializationExceptionTests(string message) : base(message)
-        {
-        }
-
-        protected InitializationExceptionTests(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
-
         [Test]
         public void VerifyInitializationExceptionMessage()
         {
-            Assert.AreEqual(nameof(InitializationExceptionTests), base.Message);
+            const string Message = "message";
+            var exception = new InitializationException(Message);
+            Assert.AreEqual(Message, exception.Message);
+            var bytes = SerializeToBytes(exception);
+            Assert.True(bytes.Length > 0);
+
+            var result = DeserializeFromBytes(bytes);
+            Assert.AreEqual(Message, result.Message);
+            Assert.Null(result.InnerException);
+        }
+
+        private static byte[] SerializeToBytes(InitializationException e)
+        {
+            using var stream = new MemoryStream();
+            new BinaryFormatter().Serialize(stream, e);
+            return stream.GetBuffer();
+        }
+
+        private static InitializationException DeserializeFromBytes(byte[] bytes)
+        {
+            using var stream = new MemoryStream(bytes);
+            return (InitializationException)new BinaryFormatter().Deserialize(stream);
         }
     }
 }
