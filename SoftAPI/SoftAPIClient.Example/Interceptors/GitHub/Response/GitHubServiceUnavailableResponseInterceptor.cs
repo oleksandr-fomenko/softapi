2accd5ccc7a4aff42d6bd34e984ea058c9a3e0c6
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-15 11:25:21 +0200

    GitHubServiceUnavailableResponseInterceptor added

---
+++ SoftAPI/SoftAPIClient.Example/Interceptors/GitHub/Response/GitHubServiceUnavailableResponseInterceptor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using SoftAPIClient.Core.Interfaces;
+
+namespace SoftAPIClient.Example.Interceptors.GitHub.Response
+{
+    public class GitHubServiceUnavailableResponseInterceptor : IResponseInterceptor
+    {
+        public void ProcessResponse(MetaData.Response response)
+        {
+            if (HttpStatusCode.ServiceUnavailable == response.HttpStatusCode)
+            {
+                throw new Exception($"Service Unavailable. Details: {response.ResponseBodyString}");
+            }
+        }
+    }
+}
