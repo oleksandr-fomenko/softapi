49bda7fda1906296be79391b9233313cd31731dd
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-15 11:35:08 +0200

    applied response interceptors to the IGitHubRepositoryProjectService

--- SoftAPI/SoftAPIClient.Example/Services/GitHub/IGitHubRepositoryProjectService.cs
+++ SoftAPI/SoftAPIClient.Example/Services/GitHub/IGitHubRepositoryProjectService.cs
@@ -4,12 +4,15 @@ using RestSharp;
 using SoftAPIClient.Attributes;
 using SoftAPIClient.Example.Core;
 using SoftAPIClient.Example.Interceptors.GitHub.Request;
+using SoftAPIClient.Example.Interceptors.GitHub.Response;
 using SoftAPIClient.Example.Models.GitHub;
 using SoftAPIClient.MetaData;
 
 namespace SoftAPIClient.Example.Services.GitHub
 {
-    [Client(DynamicUrlKey = "GitHub:Api", Path = "/repos", DynamicUrlType = typeof(DynamicConfiguration), RequestInterceptor = typeof(GitHubAuthenticationRequestInterceptor))]
+    [Client(DynamicUrlKey = "GitHub:Api", Path = "/repos", DynamicUrlType = typeof(DynamicConfiguration),
+        RequestInterceptor = typeof(GitHubAuthenticationRequestInterceptor),
+        ResponseInterceptors = new[] { typeof(GitHubAuthorizationResponseInterceptor)} )]
     public interface IGitHubRepositoryProjectService
     {
         [Log("Send GET request to 'GitHub API' for getting projects list for the owner={0} and repository={1}")]
@@ -17,11 +20,15 @@ namespace SoftAPIClient.Example.Services.GitHub
         Func<ResponseGeneric2<List<GitHubResponse>, GitHubErrorResponse>> GetProjects([PathParameter("owner")] string owner, [PathParameter("repo")] string repository);
 
         [Log("Send GET request to 'GitHub API' for getting current user-data")]
-        [RequestMapping(Method.GET, Path = "/{owner}/{repo}/projects", RequestInterceptor = typeof(GitHubRepositoryRequestInterceptor))]
+        [RequestMapping(Method.GET, Path = "/{owner}/{repo}/projects", 
+            RequestInterceptor = typeof(GitHubRepositoryRequestInterceptor))]
         Func<ResponseGeneric<List<GitHubResponse>>> GetProjects();
 
         [Log("Send POST request to 'GitHub API' for creating a next project={0}")]
-        [RequestMapping(Method.POST, Path = "/{owner}/{repo}/projects", RequestInterceptor = typeof(GitHubRepositoryRequestInterceptor))]
+        [RequestMapping(Method.POST, Path = "/{owner}/{repo}/projects", 
+            RequestInterceptor = typeof(GitHubRepositoryRequestInterceptor),
+            ResponseInterceptors = new[] { typeof(GitHubInternalServerErrorResponseInterceptor),
+                typeof(GitHubServiceUnavailableResponseInterceptor) })]
         Func<ResponseGeneric<GitHubResponse>> CreateRepositoryProject([Body] GitHubBodyRequest body);
     }
 }
