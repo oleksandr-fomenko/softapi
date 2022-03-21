4781c01e575576f9c57de26a3db4712f2cd7c5c5
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-21 12:56:39 +0200

    added missed tests for file parameter

--- SoftAPI/SoftAPIClient.Tests/FileParameterTests.cs
+++ SoftAPI/SoftAPIClient.Tests/FileParameterTests.cs
@@ -13,13 +13,28 @@ namespace SoftAPIClient.Tests
             const string contentType = "image/jpeg";
             var bytes = new byte[]{1, 2, 3};
             var fileParameter = new FileParameter(name, bytes, fileName, contentType);
+            var fileParameter2 = new FileParameter(name, bytes, fileName, contentType);
 
+            Assert.IsTrue(fileParameter.Equals(fileParameter2));
             Assert.AreEqual(name, fileParameter.Name);
             Assert.AreEqual(fileName, fileParameter.FileName);
             Assert.AreEqual(contentType, fileParameter.ContentType);
             Assert.AreEqual(bytes, fileParameter.Bytes);
             Assert.AreEqual($"Name={name}, FileName={fileName}, ContentType={contentType}", 
                 fileParameter.ToString());
+        }
+
+        [Test]
+        public void VerifyFileParameterNotEqual()
+        {
+            const string name = "testFile";
+            const string fileName = "test.jpeg";
+            const string contentType = "image/jpeg";
+            var bytes = new byte[] { 1, 2, 3 };
+            var fileParameter = new FileParameter(name, bytes, fileName, contentType);
+            var fileParameter2 = new FileParameter(name + 1, new byte[] { 1, 2, 3, 4 }, fileName + 1, contentType + 1);
+
+            Assert.IsFalse(fileParameter.Equals(fileParameter2));
         }
 
         [Test]
@@ -31,6 +46,7 @@ namespace SoftAPIClient.Tests
             Assert.IsTrue(fileParameter.Equals(fileParameter2));
             Assert.IsFalse(fileParameter.Equals(new object()));
             Assert.IsTrue(fileParameter.GetHashCode(fileParameter) == 0);
+            Assert.IsTrue(fileParameter.GetHashCode() == 0);
         }
     }
 }
