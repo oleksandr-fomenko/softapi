c188c61f95e97b6390e6e29f44d09b60a3058e41
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-21 11:56:30 +0200

    added fileAttribute and tests

---
+++ SoftAPI/SoftAPIClient/Attributes/FileAttribute.cs
@@ -0,0 +1,12 @@
+﻿using SoftAPIClient.Attributes.Base;
+
+namespace SoftAPIClient.Attributes
+{
+    [System.AttributeUsage(System.AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+    public class FileAttribute : BaseParameterAttribute
+    {
+        public FileAttribute()
+        {
+        }
+    }
+}
