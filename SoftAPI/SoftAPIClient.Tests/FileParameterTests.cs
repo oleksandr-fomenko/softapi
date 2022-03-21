ee8d0244735c2b00eddc11ca309264576058f43d
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-21 12:04:04 +0200

    added fileParameter with the tests

---
+++ SoftAPI/SoftAPIClient.Tests/FileParameterTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SoftAPIClient.Core;
+
+namespace SoftAPIClient.Tests
+{
+    public class FileParameterTests : AbstractTest
+    {
+        [Test]
+        public void VerifyFileParameter()
+        {
+            const string name = "testFile";
+            const string fileName = "test.jpeg";
+            const string contentType = "image/jpeg";
+            var bytes = new byte[]{1, 2, 3};
+            var fileParameter = new FileParameter(name, bytes, fileName, contentType);
+
+            Assert.AreEqual(name, fileParameter.Name);
+            Assert.AreEqual(fileName, fileParameter.FileName);
+            Assert.AreEqual(contentType, fileParameter.ContentType);
+            Assert.AreEqual(bytes, fileParameter.Bytes);
+            Assert.AreEqual($"Name={name}, FileName={fileName}, ContentType={contentType}", 
+                fileParameter.ToString());
+        }
+    }
+}
---
+++ SoftAPI/SoftAPIClient/Core/FileParameter.cs
@@ -0,0 +1,26 @@
+﻿namespace SoftAPIClient.Core
+{
+    public class FileParameter
+    {
+        public string Name { get; }
+        public string FileName { get; }
+        public byte[] Bytes { get; }
+        public string ContentType { get; }
+
+        public FileParameter(string name,
+            byte[] bytes,
+            string fileName,
+            string contentType)
+        {
+            Name = name;
+            FileName = fileName;
+            ContentType = contentType;
+            Bytes = bytes;
+        }
+
+        public override string ToString()
+        {
+            return $"Name={Name}, FileName={FileName}, ContentType={ContentType}";
+        }
+    }
+}
