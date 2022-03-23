919216fee4727d8528db978f137011ee93ac7471
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-23 14:41:35 +0200

    added json serializer

---
+++ SoftAPI/SoftAPIClient/Implementations/DataContractJsonObjectSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using SoftAPIClient.Core.Interfaces;
+
+namespace SoftAPIClient.Implementations
+{
+    public class DataContractJsonObjectSerializer : IObjectSerializer
+    {
+        public string Convert(object obj)
+        {
+            var serializer = new DataContractJsonSerializer(obj.GetType());
+
+            using var stream = new MemoryStream();
+            serializer.WriteObject(stream, obj);
+            var bytes = stream.ToArray();
+            return Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+        }
+    }
+}
