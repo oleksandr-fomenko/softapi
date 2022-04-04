70e6c85943347ea9ef63345ad455a0a201cfbf77
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-04-04 18:49:56 +0300

    fixed tests

--- SoftAPI/SoftAPIClient/Core/RestClient.cs
+++ SoftAPI/SoftAPIClient/Core/RestClient.cs
@@ -47,7 +47,10 @@ namespace SoftAPIClient.Core
         public RestClient AddResponseConvertor(IResponseConverter responseConvertor)
         {
             var key = responseConvertor.GetType();
-            _responseConvertors.Add(key, responseConvertor);
+            if (!_responseConvertors.ContainsKey(key))
+            {
+                _responseConvertors.Add(key, responseConvertor);
+            }
             return this;
         }
 
