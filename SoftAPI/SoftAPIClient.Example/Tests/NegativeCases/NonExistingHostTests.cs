43dbae3b4b5469962b36a1c51bfd80b4129b4fb2
Oleksandr_Fomenko <Oleksandr_Fomenko@epam.com>
2024-11-06 14:07:06 +0200

    Updated NonExistingHostTests

--- SoftAPI/SoftAPIClient.Example/Tests/NegativeCases/NonExistingHostTests.cs
+++ SoftAPI/SoftAPIClient.Example/Tests/NegativeCases/NonExistingHostTests.cs
@@ -1,25 +1,21 @@
 ﻿using NUnit.Framework;
 using SoftAPIClient.Example.Services.NegativeCases;
 using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Text;
 
 namespace SoftAPIClient.Example.Tests.NegativeCases
 {
     public class NonExistingHostTests : AbstractTest
     {
         [Test]
-        public void VerifyPostTextBody()
+        public void VerifyNonExistingHostTest()
         {
-            const string expectedErrorMessage = "No such host is known. No such host is known.";
             var response = GetService<INonExistingHostService>()
                 .SomeRequest()
                 .Invoke();
             Assert.AreEqual((HttpStatusCode)0, response.HttpStatusCode);
             Assert.NotNull(response.Exception);
-            var message = response.Exception.Message;
-            Assert.AreEqual(expectedErrorMessage, message);
+            Assert.NotNull(response.Exception.Message);
         }
     }
 }
